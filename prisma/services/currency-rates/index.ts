fc3c54a79c3870b1f2fb259decebdc34047c6d22
fedotovdev <ivan.fedotov.dev@gmail.com>
2025-05-02 00:32:55 +0300

    fix: fix some code & trigger deploy ci/cd

--- prisma/services/currency-rates/index.ts
+++ prisma/services/currency-rates/index.ts
@@ -1,9 +1,16 @@
 import { CurrencyRate as PrismaCurrencyRate } from '@prisma/client'
 import { prisma } from 'prisma/db'
+
+import logger from '~/core/logger'
 import { CurrencyRate } from '~/services/currency-rate/model'
 
 export const getCurrencyRates = async () => {
     const currencyRates = await prisma.currencyRate.findMany()
+
+    if (!currencyRates.length) {
+        logger.warn('Database is not populated with currency rates')
+        return []
+    }
 
     return currencyRates.map(mapPrismaCurrencyRateToCurrencyRate)
 }
--- src/services/currency-rate/index.ts
+++ src/services/currency-rate/index.ts
@@ -1,6 +1,27 @@
-import * as CurrencyRateService from 'prisma/services/currency-rates'
+import * as CurrencyRateDB from 'prisma/services/currency-rates'
+
+import logger from '~/core/logger'
+import { CURRENCIES } from '~/lib/consts/currencies'
+import { fetchCurrencyRate } from '~/lib/server/currency/fetchCurrencyRate'
+
+import { CurrencyRate } from './model'
 
 export const getCurrencyRates = async () => {
-    const currencyRates = await CurrencyRateService.getCurrencyRates()
+    const currencyRates = await CurrencyRateDB.getCurrencyRates()
+    if (!currencyRates.length) {
+        logger.warn('Database is not populated with currency rates')
+        return await populateCurrencyRates()
+    }
+
     return currencyRates
+}
+
+export const populateCurrencyRates = async (): Promise<CurrencyRate[]> => {
+    const results = await Promise.allSettled(CURRENCIES.map(async (currency) => fetchCurrencyRate(currency.code.toLowerCase())))
+    const rates = results
+        .filter((result): result is PromiseFulfilledResult<any> => result.status === 'fulfilled')
+        .map((result) => result.value)
+
+    await Promise.all(rates.map(async (rate) => CurrencyRateDB.upsertCurrencyRate(rate)))
+    return rates
 }
