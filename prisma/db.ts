1359968fa025cff77aa4df0cf66d41db608a069a
ivan <hi.fedotov@gmail.com>
2025-05-04 01:08:36 +0300

    remove `fetchInstagram` logs & neon adapter

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,12 +1,9 @@
-import { neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient as _PrismaClient } from '@prisma/client'
 import * as runtime from '@prisma/client/runtime/library'
-import ws from 'ws'
 
 import { env } from '~/config/env'
 
-// Defines prisma globally to not exhaust multiple connections to the datbase
+// Defines prisma globally to not exhaust multiple connections to the database
 // See https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 declare global {
     // allow global `var` declarations
@@ -14,31 +11,9 @@ declare global {
     var prisma: _PrismaClient | undefined
 }
 
-let prisma: _PrismaClient
-
-if (env.NODE_ENV === 'production' && process.env.CI !== 'true') {
-    // Configure Neon to use ws
-    neonConfig.webSocketConstructor = ws
-
-    const createNeonPrismaClient = () => {
-        const adapter = new PrismaNeon({
-            connectionString: env.DATABASE_URL,
-        })
-
-        return new _PrismaClient({
-            // @ts-ignore
-            adapter,
-        })
-    }
-
-    prisma = createNeonPrismaClient()
-} else {
-    prisma = global.prisma || new _PrismaClient()
-}
+export const prisma = global.prisma || new _PrismaClient()
+export type PrismaClient = Omit<_PrismaClient, runtime.ITXClientDenyList>
 
 if (env.NODE_ENV !== 'production') {
     global.prisma = prisma
 }
-
-export { prisma }
-export type PrismaClient = Omit<_PrismaClient, runtime.ITXClientDenyList>
--- src/app/api/instagram/oauth/callback/route.ts
+++ src/app/api/instagram/oauth/callback/route.ts
@@ -2,7 +2,6 @@ import { tasks } from '@trigger.dev/sdk/v3'
 import { NextResponse } from 'next/server'
 
 import { env } from '~/config/env'
-import logger from '~/core/logger'
 import { createRouteHandler } from '~/core/middleware/with-route-handler'
 import { getLongLivedAccessToken, getMe, getShortLivedAccessToken } from '~/lib/server/instagram/api'
 import { extractBusinessIdFromConversations } from '~/lib/server/instagram/lib/extractBusinessIdFromConversations'
@@ -17,23 +16,18 @@ export const GET = createRouteHandler()({ auth: false }, async ({ req: request }
     const code = searchParams.get('code')
     const state = searchParams.get('state')
 
-    logger.info(`Got state and code`, { state, code })
+    if (!code) {
+        return NextResponse.json({ data: null, error: 'No code provided' }, { status: 400 })
+    }
 
     if (state && code && 'organizationId' in JSON.parse(state)) {
         const organizationId = JSON.parse(state).organizationId
-        logger.info(`Got organization id`, { organizationId })
 
         const organization = await getOrganizationById(organizationId)
-        logger.info(`Got organization`, { organizationId, organization })
 
         const shortLivedAccessToken = await getShortLivedAccessToken(code)
-        logger.info(`Got short lived access token`, { organizationId, shortLivedAccessToken })
-
         const longLivedAccessToken = await getLongLivedAccessToken(shortLivedAccessToken.access_token, env.INSTAGRAM_APP_SECRET)
-        logger.info(`Got long lived access token`, { organizationId, longLivedAccessToken })
-
         const me = await getMe(longLivedAccessToken.access_token, shortLivedAccessToken.user_id)
-        logger.info(`Got me`, { organizationId, me })
 
         const integration = await addInstagramIntegration(organizationId, {
             accessToken: longLivedAccessToken.access_token,
@@ -57,10 +51,6 @@ export const GET = createRouteHandler()({ auth: false }, async ({ req: request }
             websiteUrl: organization.websiteUrl ?? '',
             dialogs: conversations,
         })
-    }
-
-    if (!code) {
-        return NextResponse.json({ data: null, error: 'No code provided' }, { status: 400 })
     }
 
     return NextResponse.json({ data: null, error: null }, { status: 302, headers: { Location: env.BASE_URL } })
